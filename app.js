55beb712870ba0e6a160be198357c1e1bbc480b7
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2024-09-14 22:04:02 +0300

    aa

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let container = document.querySelector('.container')
+ 
+ let container = document.querySelector('.container')
 let container1 = document.querySelector('.container1')
 let container2 = document.querySelector('.container2')
 let container3 = document.querySelector('.container3')
